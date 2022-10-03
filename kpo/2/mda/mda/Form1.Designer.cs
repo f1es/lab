ef7c34d2b7eae33cc7e8bd5863cb6fea99281361
Ilya Flex <anotherfles@gmail.com>
2022-10-03 23:08:40 +0300

    kpo2

---
+++ kpo/2/mda/mda/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace mda
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Big Button";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(265, 12);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(426, 155);
+            this.Input.TabIndex = 1;
+            this.Input.TextChanged += new System.EventHandler(this.Input_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Output:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.comboBox1.Location = new System.Drawing.Point(38, 144);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(170, 23);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "№";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(709, 267);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "¯\\_(ツ)_/¯";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox Input;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Label label4;
+    }
+}
---
+++ kpo/2/mda/mda/Form1.cs
@@ -0,0 +1,149 @@
+namespace mda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button1_Click;
+        }
+        string str2;
+        private void Input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            str2 = Input.Text;
+
+            switch(Convert.ToInt32(comboBox1.SelectedItem))
+            {
+                case 1:
+                    str2 = N1(str2);
+                    label2.Text = str2;
+                    break;
+                case 2:
+                    str2 = N2(str2);
+                    label2.Text = str2;
+                    break;
+                case 3:
+                    str2 = N3(str2);
+                    label2.Text = str2;
+                    break;
+                case 4:
+                    str2 = N4(str2);
+                    label2.Text = str2;
+                    break;
+            }
+        }
+
+        string N1(string str2)
+        {
+            string str = "";
+            int i = str2.Length;
+            if (str2.Length > 2)
+            {
+                str += "first element : ";str += str2[0];
+                str += " middle element : "; str += str2[str2.Length / 2];
+                str += " last element : "; str += str2[i - 1];
+                return str;
+            }
+            if (str2.Length == 2)
+            {
+                str += "first element : "; str += str2[0];
+                str += " last element : "; str += str2[i - 1];
+                return str;
+            }
+
+            return str;
+        }
+
+        string N2(string str)
+        {
+            int counter = 0;
+            for (int i = 0; i < (str.Length) - 2; i++)
+            {
+                if (str[i] == 'a' && str[i + 1] == 'b' && str[i + 2] == 'a')
+                {
+                    counter++;
+                }
+            }
+
+            string str2;
+            str2 = "aba count : " + Convert.ToString(counter);
+            return str2;
+        }
+        string N3(string str)
+        {
+            string[] words = str.Split(' ').ToArray();
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                for(int j = 0; j < words.Length; j++)
+                if (words[i][0] < words[j][0])
+                    {
+                        string temp = words[i];
+                        words[i] = words[j];
+                        words[j] = temp;
+                    }
+            }
+            string str2 = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                str2 += ' ';
+                str2 += words[i];
+                //str2 += ' ';
+            }
+
+            str2 += Environment.NewLine;
+
+            int count = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    if (words[i][j] == 'à') count++;
+                }
+
+                if (count >= 2)
+                {
+                    str2 += ' ';
+                    str2 += words[i];
+                }
+                count = 0;
+            }
+
+            return str2;
+        }
+
+        string N4(string str)
+        {
+            string str2 = "";
+            string[] words = str.Split(' ').ToArray(); 
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                try
+                {
+                   int a = int .Parse(words[i]);
+
+                    str2 += " <";
+                   str2 += Convert.ToString(a);
+                    str2 += "> ";
+                }
+                catch   
+                {
+                    str2 += words[i];
+                }
+            }
+
+            return str2;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
