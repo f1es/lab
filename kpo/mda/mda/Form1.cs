b8e6cb3219088431fea450ad93e023639459d2ca
Ilya Flex <anotherfles@gmail.com>
2022-10-06 22:02:41 +0300

    kpo2,3

--- kpo/mda/mda/Form1.cs
+++ kpo/mda/mda/Form1.cs
@@ -3,9 +3,36 @@ namespace mda
     public partial class Form1 : Form
     {
         ZNAK[] BOOK = new ZNAK[8];
+
         public Form1()
         {
             InitializeComponent();
+
+            BOOK[0].name.Name = "Александр";
+            BOOK[0].name.Surname = "Иванов";
+            BOOK[0].zodiac = "Водолей";
+            int[] a = { 15, 7, 2006 };
+            BOOK[0].bdate = a;
+            //
+            BOOK[1].name.Name = "Сергей";
+            BOOK[1].name.Surname = "Шайко";
+            BOOK[1].zodiac = "Стрелец";
+            int[] g = { 6, 9, 2007 };
+            BOOK[1].bdate = g;
+            //
+            BOOK[2].name.Name = "Иван";
+            BOOK[2].name.Surname = "Иванов";
+            BOOK[2].zodiac = "Скорпион";
+            int[] c = { 12, 12, 2004 };
+            BOOK[2].bdate = c;
+            //
+            BOOK[3].name.Name = "Алексей";
+            BOOK[3].name.Surname = "Алексеев";
+            BOOK[3].zodiac = "Козерог";
+            int[] d = { 7, 8, 2007 };
+            BOOK[3].bdate = d;
+
+            //int knopka = 0;
             button1.Click += button1_Click;
         }
         string str2;
@@ -14,8 +41,9 @@ namespace mda
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object? sender, EventArgs e)
         {
+            //MessageBox.Show(":(");
             //str2 = Input.Text;
             int number = Convert.ToInt32(comboBox2.SelectedItem);
             label2.Text = " ";
@@ -31,9 +59,57 @@ namespace mda
                     break;
                 case "Ввести день рождения":
                     BOOK[number] = AddBDay(BOOK[number], Input.Text);
+                    ZNAK temp;
+                    for (int i = 0; i < 8; i++)
+                        for (int j = 0; j < 8; j++) 
+                        {
+                            //int[] a1 = new int[3];
+                            // int[] a2 = new int[3];
+                            //a1 = BOOK[j].bdate;
+                            //a2 = BOOK[i].bdate;
+                            if (BOOK[j].bdate != null && BOOK[i].bdate != null)
+                            {
+                                if (BOOK[j].bdate[2] < BOOK[i].bdate[2])
+                                {
+                                    temp = BOOK[i];
+                                    BOOK[i] = BOOK[j];
+                                    BOOK[j] = temp;
+                                }
+                                else if (BOOK[j].bdate[2] == BOOK[i].bdate[2] && BOOK[j].bdate[1] < BOOK[i].bdate[1])
+                                {
+                                    temp = BOOK[i];
+                                    BOOK[i] = BOOK[j];
+                                    BOOK[j] = temp;
+                                }
+                                else if (BOOK[j].bdate[2] == BOOK[i].bdate[2] && BOOK[j].bdate[1] == BOOK[i].bdate[1] && BOOK[j].bdate[0] < BOOK[i].bdate[0])
+                                {
+                                    temp = BOOK[i];
+                                    BOOK[i] = BOOK[j];
+                                    BOOK[j] = temp;
+                                }
+                            }
+                        }
                     break;
                 case "Информация по фамилии":
-                    // str2 = N4(str2);
+                    int[] bb = { 0, 0, 0 };
+                    for (int i = 0; i < BOOK.Length; i++)
+                    {
+                        if (Input.Text == BOOK[i].name.Surname)
+                        {
+                            if (BOOK[i].bdate != null) bb = BOOK[i].bdate;
+                            str2 += BOOK[i].name.Name + ' ' + BOOK[i].name.Surname + " | " + BOOK[i].zodiac + " | ";
+
+                            string day = "", month = "";
+                            if (bb[0] < 10) day += '0' + Convert.ToString(bb[0]);
+                            else day += Convert.ToString(bb[0]);
+
+                            if (bb[1] < 10) month += '0' + Convert.ToString(bb[1]);
+                            else month += Convert.ToString(bb[1]);
+
+                            if (BOOK[i].bdate != null) str2 += Convert.ToString(day) + '.' + Convert.ToString(month) + '.' + Convert.ToString(bb[2]) + Environment.NewLine;
+                            else str2 += Environment.NewLine;
+                        }
+                    }
                     label2.Text = str2;
                     break;
                 case "Информация о всех":
@@ -41,11 +117,11 @@ namespace mda
                     for (int i = 0; i < BOOK.Length - 1; i++)
                     {
                         if (BOOK[i].bdate != null) b = BOOK[i].bdate;
-                        str2 += BOOK[i].name.Name + ' ' + BOOK[i].name.Surname + ' ' + BOOK[i].zodiac + ' ';
+                        str2 += BOOK[i].name.Name + ' ' + BOOK[i].name.Surname + " | " + BOOK[i].zodiac + " | ";
 
                         string day = "", month = "";
                         if (b[0] < 10) day += '0' + Convert.ToString(b[0]);
-                        else day += Convert.ToString(b[1]);
+                        else day += Convert.ToString(b[0]);
 
                         if (b[1] < 10) month += '0' + Convert.ToString(b[1]);
                         else month += Convert.ToString(b[1]);
---
+++ kt/2/kt2.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+int hash_function(string str);
+//string multi_hash(string str, int id);
+
+int main()
+{
+	string hash_table[200];
+	int a[3];
+	int b[3];
+	//a = b;
+	string str;
+	//cin >> str;
+
+	int id = hash_function(str), n = 1;
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> str;
+		id = hash_function(str);
+		while (1)
+		{
+			if (hash_table[id] == "")
+			{
+				hash_table[id] = str;
+				break;
+			}
+			else id *= ++n;
+		}
+	}
+
+	//hash_table = hash_function(hash_table, str);
+	for (int i = 0; i < 200; i++)
+	{
+		if (hash_table[i] != "") cout << hash_table[i] << endl;
+	}
+	//cout << hash_table[hash_function(str)] << endl;
+}
+
+int hash_function(string str)
+{
+	int id = 0;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		id += str[i];
+	}
+
+	id %= 200;
+	//hash_table[id] = str;
+
+	return id;
+}
