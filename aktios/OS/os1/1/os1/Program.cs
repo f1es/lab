c6b150003b722e7ae4f7ce9a93eacc13006590f4
Ilya Flex <anotherfles@gmail.com>
2022-11-24 17:47:02 +0300

    DENIS

---
+++ aktios/OS/os1/1/os1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Console.WriteLine("Hello, World!");
+string path = @"X:\vs\git\lab\aktios\OS\os1\source.txt";
+
+string str;
+//str = Console.ReadLine();
+str = File.ReadAllText(path);
+var str2 = str.Split(' ');
+
+double a, b, c;
+a = Convert.ToDouble(str2[0]);
+b = Convert.ToDouble(str2[1]);
+c = Convert.ToDouble(str2[2]);
+
+File.Delete(path);
+
+string res = Convert.ToString(func1(a, b, c));
+File.WriteAllText(path, res);
+
+double func1(double a, double b, double c)
+{
+    return a / b * Math.Sqrt(c);
+}
---
+++ aktios/OS/os1/2/os1/Program.cs
@@ -0,0 +1,17 @@
+﻿string path = @"X:\vs\git\lab\aktios\OS\os1\source.txt";
+
+string str = "";
+//str = Console.ReadLine();
+str = File.ReadAllText(path);
+
+double s;
+s = Convert.ToDouble(str);
+
+File.Delete(path);
+string res = Convert.ToString(func1(s));
+File.WriteAllText(path, res);
+
+double func1(double s)
+{
+    return Math.Pow(s, 5);
+}
