4757c5a4d51e6d02e6b231e8c5ff4efb00c4c7df
Ilya Flex <anotherfles@gmail.com>
2022-11-30 09:57:49 +0300

    kt

---
+++ kt/6/BasicSharp-master/BasicSharp.Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace BasicSharp.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string file in Directory.GetFiles(Path.Combine(Environment.CurrentDirectory, "Tests"), "*.bas"))
+            {
+                Interpreter basic = new Interpreter(File.ReadAllText(file));
+                basic.printHandler += Console.WriteLine;
+                basic.inputHandler += Console.ReadLine; 
+                try
+                {
+                    basic.Exec();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("BAD");
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                Console.WriteLine("OK");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ kt/6/BasicSharp-master/BasicSharp.Test/Tests/operators.bas
@@ -0,0 +1,21 @@
+assert 1 = 1
+assert 3 = 2 + 1
+assert 2 = 5 - 3
+assert 6 = 3 * 2
+assert 2 = 6 / 3
+assert -1 = 1 - 2
+assert 1 <> 2
+assert Not 1 <> 1
+assert 3 > 2
+assert 2 < 3
+assert 2 >= 1
+assert 1 >= 1
+assert 1 < 2
+assert 1 <= 1
+assert 1 And 1
+assert 1 Or 0
+assert "abc" = "abc"
+assert "abc" <> "abc1"
+assert 1 = Not 0
+assert 0 = Not 1
+  
