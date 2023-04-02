c39fbcf3ea4a94090f7d185a14a1ab4e9e1e3d8a
Ilya Flex <anotherfles@gmail.com>
2023-04-03 01:42:27 +0300

    ti

---
+++ sem4/TI/4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ti4
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string words = "CRYPTOGRAPHY AND DATA SECURITY", key = "MOUSE";
+            Console.WriteLine(Vijener.VijenerEncrypt(words,key));
+
+            string encr = Vijener.VijenerEncrypt(words,key);
+            Console.WriteLine(Vijener.VijenarDecrypt(encr,key));
+        }
+    }
+}
