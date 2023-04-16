7b90e0e704c74bc044294a35f3dc0b1f5aefca99
Ilya Flex <anotherfles@gmail.com>
2023-04-17 00:05:13 +0300

    ti5

---
+++ sem4/TI/5/ti5/ti5/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Idea 
+{
+    class Program
+    {
+        static void Main()
+        {
+            Idea idea = new Idea("12345678soidfhiuoshf iuhfiulhsjfsioejdo isejfhoishfjiosjfoi shfos ", true);
+            string str = "zdarova ";
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(str);
+            var bb = idea.crypt(bytes);
+
+            Console.WriteLine(System.Text.Encoding.ASCII.GetString(bb));
+
+            Idea idea1 = new Idea("12345678soidfhiuoshf iuhfiulhsjfsioejdo isejfhoishfjiosjfoi shfos ", false);
+            var aa = idea1.crypt(bytes);
+
+            Console.WriteLine(System.Text.Encoding.ASCII.GetString(aa));
+        }
+    }
+}
