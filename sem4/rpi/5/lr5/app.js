0bd77894870ae648668a7c3a5662b6f8a4f4d0b7
Ilya Flex <anotherfles@gmail.com>
2023-05-15 23:34:19 +0300

    web,rpi

--- sem4/rpi/5/lr5/app.js
+++ sem4/rpi/5/lr5/app.js
@@ -1,25 +1,37 @@
 const express = require("express");
-   
-const app = express();
-   
-// создаем парсер для данных 
-const urlencodedParser = express.urlencoded({extended: false});
 
-let result ; 
-  
+const app = express();
+
+// создаем парсер для данных 
+const urlencodedParser = express.urlencoded({ extended: false });
+
+let result;
+
 app.get("/", function (request, response) {
     response.sendFile(__dirname + "/index.html");
 });
 app.post("/", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
+    if (!request.body) return response.sendStatus(400);
     console.log(request.body);
 
+    let result = 0;
     // для переключателей
-    result = Number(request.body.question1);
+    result += Number(request.body.question1);
     // для флажков
-    if(request.body.question21 == "1" && request.body.question22 == "1") result ++;
+    if (request.body.question21 == "1" && request.body.question22 == "1") result++;
+    result += Number(request.body.question3);
+    // if (Number(request.body.question4) == 1) result++;
+    // if (Number(request.body.question5) == 1) result++;
+    // if (Number(request.body.question6) == 1) result++;
+    result += Number(request.body.question4);
+    result += Number(request.body.question5);
+    result += Number(request.body.question6);
+    if (request.body.question71 == "1" && request.body.question72 == "1") result++;
+    // result += Number(request.body.question8)
+    // result += Number(request.body.question9)
+    // result += Number(request.body.question10)
 
-    response.send(`Привет ${request.body.userName} из ${request.body.userGroup} --- твой результат  ${result} из 2`);
+    response.send(`Привет ${request.body.userName} из ${request.body.userGroup} --- твой результат  ${result} из 10`);
 });
-   
-app.listen(3000, ()=>console.log("Сервер запущен..."));
+
+app.listen(3000, () => console.log("Сервер запущен..."));
