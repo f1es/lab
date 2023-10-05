c6f1481c1abc77909607de935541afbc2e5683c6
fles <anotherfles@gmail.com>
2023-10-05 20:08:33 +0300

    daafafa

---
+++ sem5/sp/3/sp3/sp3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+//using Gdk;
+using Gtk;
+
+
+public class GtkApplication : Window
+{
+    public GtkApplication() : base(WindowType.Toplevel)
+    {
+        
+    }
+
+    protected void OnDeleteEvent(Object sender, EventArgs e)
+    {
+        Application.Quit();
+        
+    }
+
+
+    public static void Main()
+    {
+        //Console.WriteLine("HelloWorld");
+        Application.Init();
+        Window windw = new Window("fdfdf");
+        //Create a label and put some text in it.
+
+        string str = Console.ReadLine();
+        Label myLabel = new Label();
+        myLabel.Text = str;
+        Entry entry = new Entry();
+        
+
+
+        //Add the label to the form
+        windw.Add(entry);
+        
+        windw.ShowAll();
+        Application.Run();
+
+
+
+    }
+
+    public bool Search(int number)
+    { 
+    }
+}
---
+++ sem5/web/1/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const app = express();
+
+// создаем парсер для данных 
+const urlencodedParser = express.urlencoded({ extended: false });
+
+let result;
+
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/todo.html");
+});
+app.post("/", urlencodedParser, function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+});
+/*
+    let result = 0;
+    // для переключателей
+    result += Number(request.body.question1);
+    // для флажков
+    if (request.body.question21 == "1" && request.body.question22 == "1") result++;
+    result += Number(request.body.question3);
+    result += Number(request.body.question4);
+    result += Number(request.body.question5);
+    result += Number(request.body.question6);
+    if (request.body.question71 == "1" && request.body.question72 == "1") result++;
+    result += Number(request.body.question8);
+    result += Number(request.body.question9);
+    result += Number(request.body.question10);
+
+    response.send(`Привет ${request.body.userName} из ${request.body.userGroup} --- твой результат  ${result} из 10`);
+});
+*/
+
+app.listen(3000, () => console.log("Сервер запущен..."));
