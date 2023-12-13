ee94452c6d069664b049c619d73ef63905f5be08
fles <anotherfles@gmail.com>
2023-12-13 16:02:56 +0300

    riat5

--- sem5/riat/5/app.py
+++ sem5/riat/5/app.py
@@ -1,10 +1,53 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+inventory = [
+    {'element': "el"}
+]
+
+
 @app.route('/inventory')
-def get_products():
-    return 'List of inventory'
+def get_elements():
+    return jsonify(inventory)
+
+
+@app.route('/inventory/add', methods=['POST'])
+def create_element():
+    data = request.get_json()
+    element = data.get('element')
+    inventory_element = {'element': element}
+    inventory.append(inventory_element)
+    return jsonify({'message': 'Element created successfully'}), 201
+
+
+@app.route('/inventory/<int:element_id>', methods=['GET'])
+def get_element(element_id):
+    if element_id >= len(inventory):
+        return jsonify({'error': 'Element not found'}), 404
+    return jsonify(inventory[element_id])
+
+
+@app.route('/inventory/<int:element_id>', methods=['PUT'])
+def update_element(element_id):
+    if element_id >= len(inventory):
+        return jsonify({'error': 'Element not found'}), 404
+    data = request.get_json()
+    inventory[element_id]['element'] = data.get('element')
+    return jsonify({'message': 'Element updated successfully'})
+
+
+@app.route('/inventory/<int:element_id>', methods=['DELETE'])
+def delete_element(element_id):
+    if element_id >= len(inventory):
+        return jsonify({'error': 'Element not found'}), 404
+    del inventory[element_id]
+    return jsonify({'message': 'Element deleted successfully'})
+
+
+@app.route('/inventory/len')
+def inventory_len():
+    return jsonify({'message': f'Inventory countains, {len(inventory)}, elements'})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
