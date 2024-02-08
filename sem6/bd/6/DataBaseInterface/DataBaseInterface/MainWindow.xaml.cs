f9dcdfcaa0289cd187fb07b3fa3e4ef9ecd4bf6b
Ilya Flex <anotherfles@gmail.com>
2024-02-08 23:46:18 +0300

    bd6

---
+++ sem6/bd/6/DataBaseInterface/DataBaseInterface/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBaseInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> names = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            names = new List<string>() { "John", "Alex", "Sam"};
+            data.ItemsSource = names;
+            listbox.ItemsSource = names;
+        }
+    }
+}
