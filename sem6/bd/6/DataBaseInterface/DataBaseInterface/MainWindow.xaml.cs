a925059cdf1c6a425c55665df1253815a1dd7d3b
Ilya Flex <anotherfles@gmail.com>
2024-02-11 15:57:50 +0300

    bd

--- sem6/bd/6/DataBaseInterface/DataBaseInterface/MainWindow.xaml.cs
+++ sem6/bd/6/DataBaseInterface/DataBaseInterface/MainWindow.xaml.cs
@@ -22,58 +22,49 @@ namespace DataBaseInterface
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<Table> tables = new ObservableCollection<Table>();
-
+        //ObservableCollection<DataTable> tables = new ObservableCollection<DataTable>();
+        private Dictionary<string, DataTable> tablesNamesDictionary = new Dictionary<string, DataTable>();
+        private ObservableCollection<string> tablesNames = new ObservableCollection<string>();
         public MainWindow()
         {
             InitializeComponent();
 
             //names = new ObservableCollection<string>() { "John", "Alex", "Sam",};
             //TablesListView.ItemsSource = names;
-            
+            TablesListView.ItemsSource = tablesNames;
 
-            DataTable dataTable1 = new DataTable();
-            dataTable1.Columns.Add("id");
-            dataTable1.Columns.Add("name");
-            dataTable1.Columns.Add("surname");
-            dataTable1.Rows.Add(new object[] { 1, "Kirill", "Bebrikov" });
-            dataTable1.Rows.Add(new object[] { 2, "Artyom", "Grib" });
+            DataTable Workers = new DataTable();
+            Workers.Columns.Add("id");
+            Workers.Columns.Add("name");
+            Workers.Columns.Add("birthday");
+            Workers.Columns.Add("age");
+            Workers.Columns.Add("speciality_id");
+            Workers.Rows.Add(new object[] { 1, "Kirill", "1999-5-5", 22, 1});
+            Workers.Rows.Add(new object[] { 2, "Mihail", "1998-6-8", 21, 4 });
+            Workers.Rows.Add(new object[] { 3, "John", "1999-1-5", 22, 3 });
+            Workers.Rows.Add(new object[] { 4, "Ivan", "2000-9-7", 22, 1 });
+            Workers.Rows.Add(new object[] { 5, "Petr", "2001-12-12", 22, 2 });
 
-            DataTable dataTable2 = new DataTable();
-            dataTable2.Columns.Add("id");
-            dataTable2.Columns.Add("name");
-            dataTable2.Columns.Add("birthday");
-            dataTable2.Columns.Add("age");
-            dataTable2.Rows.Add(new object[] { 1, "gleb", "16-16-2016", 22 });
+            DataTable Speciality = new DataTable();
+            Speciality.Columns.Add("id");
+            Speciality.Columns.Add("name");
+            Speciality.Columns.Add("salary");
+            Speciality.Rows.Add(new object[] { 1, "slesar", 200});
+            Speciality.Rows.Add(new object[] { 2, "yborshik", 150 });
+            Speciality.Rows.Add(new object[] { 3, "youtuber", 400 });
+            Speciality.Rows.Add(new object[] { 4, "programmer", 500 });
+            Speciality.Rows.Add(new object[] { 5, "mehanik", 300 });
 
-            Table table = new Table();
-            table.Name = "test table";
-            table.DataTable = dataTable1;
-
-            Table table1 = new Table();
-            table.Name = "test table2";
-            table1.DataTable = dataTable2; 
-
-            ObservableCollection<Table> tables = new ObservableCollection<Table>();
-
-            tables.Add(table);
-            tables.Add(table1);
-            TablesListView.ItemsSource = tables;
-
-            foreach (var item in TablesListView.Items)
-            {
-                //((ListViewItem)item).DataContext = "123";
-            }
-            //TablesListView.SelectedItem;
-            //dataGrid.ItemsSource = dataTable.DefaultView;
+            tablesNames.Add("Workers");
+            tablesNames.Add("Speciality");
+            tablesNamesDictionary.Add("Workers", Workers);
+            tablesNamesDictionary.Add("Speciality", Speciality);
         }
 
-        private void AddButton_Click(object sender, RoutedEventArgs e)
+        private void ViewTableButton_Click(object sender, RoutedEventArgs e)
         {
-            dataGrid.ItemsSource = ((Table)TablesListView.SelectedItem).DataTable.DefaultView;
-            
-            //names.Add(textbox.Text);
-            //ObservableCollection<string> s = new ObservableCollection<string>();
+            if (TablesListView.SelectedItem != null)
+                dataGrid.ItemsSource = tablesNamesDictionary[((string)TablesListView.SelectedItem)].DefaultView;
         }
 
         private void TablesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
