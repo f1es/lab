a1eb872d1a4a624c66b5510a44b8e7250aaa2358
fles <anotherfles@gmail.com>
2024-02-11 14:20:34 +0300

    bd

--- sem6/bd/6/DataBaseInterface/DataBaseInterface/MainWindow.xaml.cs
+++ sem6/bd/6/DataBaseInterface/DataBaseInterface/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +22,63 @@ namespace DataBaseInterface
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<string> names = new List<string>();
+        ObservableCollection<Table> tables = new ObservableCollection<Table>();
+
         public MainWindow()
         {
             InitializeComponent();
 
-            names = new List<string>() { "John", "Alex", "Sam"};
-            data.ItemsSource = names;
-            listbox.ItemsSource = names;
+            //names = new ObservableCollection<string>() { "John", "Alex", "Sam",};
+            //TablesListView.ItemsSource = names;
+            
+
+            DataTable dataTable1 = new DataTable();
+            dataTable1.Columns.Add("id");
+            dataTable1.Columns.Add("name");
+            dataTable1.Columns.Add("surname");
+            dataTable1.Rows.Add(new object[] { 1, "Kirill", "Bebrikov" });
+            dataTable1.Rows.Add(new object[] { 2, "Artyom", "Grib" });
+
+            DataTable dataTable2 = new DataTable();
+            dataTable2.Columns.Add("id");
+            dataTable2.Columns.Add("name");
+            dataTable2.Columns.Add("birthday");
+            dataTable2.Columns.Add("age");
+            dataTable2.Rows.Add(new object[] { 1, "gleb", "16-16-2016", 22 });
+
+            Table table = new Table();
+            table.Name = "test table";
+            table.DataTable = dataTable1;
+
+            Table table1 = new Table();
+            table.Name = "test table2";
+            table1.DataTable = dataTable2; 
+
+            ObservableCollection<Table> tables = new ObservableCollection<Table>();
+
+            tables.Add(table);
+            tables.Add(table1);
+            TablesListView.ItemsSource = tables;
+
+            foreach (var item in TablesListView.Items)
+            {
+                //((ListViewItem)item).DataContext = "123";
+            }
+            //TablesListView.SelectedItem;
+            //dataGrid.ItemsSource = dataTable.DefaultView;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = ((Table)TablesListView.SelectedItem).DataTable.DefaultView;
+            
+            //names.Add(textbox.Text);
+            //ObservableCollection<string> s = new ObservableCollection<string>();
+        }
+
+        private void TablesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
--- sem6/bd/6/DataBaseInterface/DataBaseInterface/obj/Debug/net6.0-windows/App.g.i.cs
+++ sem6/bd/6/DataBaseInterface/DataBaseInterface/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace DataBaseInterface {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace DataBaseInterface {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public static void Main() {
             DataBaseInterface.App app = new DataBaseInterface.App();
             app.InitializeComponent();
