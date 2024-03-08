ecd16c64361025ffa5b3e59a9924833a8376dd5c
Ilya Flex <anotherfles@gmail.com>
2024-03-08 20:39:57 +0300

    rv1

--- sem6/rv/kmeans.cpp
+++ sem6/rv/kmeans.cpp
@@ -4,337 +4,350 @@
 #include <chrono>
 #include <random>
 #include <fstream>
+#include <string>
+#include <filesystem>
 
+//Made by ilya flex
 using namespace std;
 
-const int countOfPoints = 16; // Кол-во точек
-const int countOfDimensions = 2; // Кол-во измерений
-const int countOfClusters = 3; // Кол-во кластеров
+const int PointsCount = 60000; // Кол-во точек      60 000
+const int DimensionsCount = 11; // Кол-во измерений 11
+const int ClustersCount = 12; // Кол-во кластеров   12
+int maxIterations = 20; // Максимальное Кол-во итераций
+string outputFile = "X:\\vs\\git\\ootpisp2\\siplasplas\\siplasplas\\IterationsTime.txt";
 
-class Point 
+class Point
 {
 private:
-    float _position[countOfDimensions]; // Координаты точки
+    vector<float> _position; // Координаты точки
 public:
+    Point()
+    {
+        SetToZero();
+    }
+    Point(vector<float> position)
+    {
+        SetPosition(position);
+    }
     // Получить координаты точки
-    float* GetPosition()
+    vector<float>& GetPosition()
     {
         return _position;
     }
     // Установить координаты точки
-    void SetPosition(float* position)
+    void SetPosition(vector<float> position)
     {
-        for (int i = 0; i < countOfDimensions; i++)
+        _position.clear();
+
+        for (int i = 0; i < DimensionsCount; i++)
         {
-            _position[i] = position[i];
+            _position.push_back(position[i]);
         }
     }
-    // Изменить координат всех измерений на 0
+    // Изменить координаты всех измерений на 0
     void SetToZero()
     {
-        for (int i = 0; i < countOfDimensions; i++)
+        _position.clear();
+
+        for (int i = 0; i < DimensionsCount; i++)
         {
-            _position[i] = 0;
+            _position.push_back(0);
         }
+    }
+    // Вывести координаты точки
+    void PrintCordinates()
+    {
+        for (int i = 0; i < DimensionsCount; i++)
+        {
+            cout << i + 1 << ":" << _position[i] << endl;
+        }
+    }
+    // Оператор сравнения двух точек
+    bool operator == (Point point) const
+    {
+        for (int i = 0; i < DimensionsCount; i++)
+        {
+            if (_position[i] != point.GetPosition()[i])
+                return false;
+        }
+
+        return true;
     }
 };
 
-class Cluster 
+class Cluster
 {
 private:
     Point _centerPoint; // Центр кластера
     vector<Point> _points; // Точки кластера
 public:
+    Cluster()
+    {
+        _centerPoint.SetToZero();
+    }
+    Cluster(Point center)
+    {
+        _centerPoint = center;
+    }
     // Получить центр кластера
     Point GetCenterPoint()
     {
         return _centerPoint;
     }
     // Установить центр кластера
-    void SetCenterPoint(Point newCenterPoint)
+    void SetCenterPoint(Point centerPoint)
     {
-        _centerPoint = newCenterPoint;
+        _centerPoint = centerPoint;
     }
     // Добавить точку в кластер
     void AddPoint(Point point)
     {
         _points.push_back(point);
     }
-    // Вывести все точки кластера
-    void ViewPoints()
-    {
-        cout << "cluster have " << _points.size() << " points" << endl;
-        for (int i = 0; i < _points.size(); i++)
-        {
-            cout << i << " point on position" << endl;
-            for (int j = 0; j < countOfDimensions; j++)
-            {
-                cout << j << " dimension - " << _points[i].GetPosition()[j] << endl;
-            }
-        }
-    }
     // Очистить точки кластера
     void ClearPoints()
     {
         _points.clear();
+    }
+    // Вывести все точки кластера
+    void ViewPoints()
+    {
+        cout << "cluster have " << _points.size() << " points" << endl;
+        cout << "   center" << endl;
+        _centerPoint.PrintCordinates();
+        for (int i = 0; i < _points.size(); i++)
+        {
+            cout << "   point " << i + 1 << endl;
+            _points[i].PrintCordinates();
+        }
     }
     // Получить количество точек
     int GetPointsCount()
     {
         return _points.size();
     }
-    // Посчитать центр кластера
-    Point CalculateCenter()
+    // Обновить центр кластера исходя из пренадлежащих ему точек
+    void UpdateCenterPoint()
     {
+        if (GetPointsCount() == 0) // Если у кластера нет точек то ничего не делаем
+            return;
+
         Point pointsSum;
-        pointsSum.SetToZero();
-        for (int i = 0; i < GetPointsCount(); i++)
+        for (int i = 0; i < GetPointsCount(); i++) // Считаем сумму измерений всех точек
         {
-            for (int j = 0; j < countOfDimensions; j++)
+            for (int j = 0; j < DimensionsCount; j++)
             {
                 pointsSum.GetPosition()[j] += _points[i].GetPosition()[j];
             }
         }
 
-        Point newCenter;
-        newCenter.SetToZero();
-        for (int i = 0; i < countOfDimensions; i++)
+        Point newCenterPoint;
+        for (int i = 0; i < DimensionsCount; i++) // Делим сумму каждого измерения на количество измерений
         {
-            newCenter.GetPosition()[i] = pointsSum.GetPosition()[i] / GetPointsCount();
+            newCenterPoint.GetPosition()[i] = pointsSum.GetPosition()[i] / GetPointsCount();
         }
 
-        return newCenter;
+        SetCenterPoint(newCenterPoint); // Устанавливаем новый центр кластера
+    }
+};
+
+class KMeans
+{
+private:
+    vector<Point> _points;
+    vector<Cluster> _clusters;
+    // Считает расстояние между точками
+    float CalculateDistance(Point firstPoint, Point secondPoint)
+    {
+        float distance = 0;
+        for (int i = 0; i < DimensionsCount; i++)    // Считает расстояние между каждым измерением точек
+            distance += pow(firstPoint.GetPosition()[i] - secondPoint.GetPosition()[i], 2);
+        distance = sqrt(distance);
+        return distance;
+    }
+    // Присваиваем точки к ближайшему кластеру
+    void AssignPoints()
+    {
+        for (int i = 0; i < ClustersCount; i++) //Очищаем существующие точки кластеров, чтобы их переприсвоить
+            _clusters[i].ClearPoints();
+
+        for (int i = 0; i < PointsCount; i++)
+        {
+            float minDistance = INFINITY;
+            int closestClusterIndex = 0;
+            for (int j = 0; j < ClustersCount; j++)
+            {
+                float distance = CalculateDistance(_points[i], _clusters[j].GetCenterPoint()); // Считаем расстояние между точкой и центром кластера
+                if (distance < minDistance) // Если расстояние до текущего кластера меньше чем расстояние до предыдущих, запоминаем расстояние и индекс кластера
+                {
+                    minDistance = distance;
+                    closestClusterIndex = j;
+                }
+            }
+            _clusters[closestClusterIndex].AddPoint(_points[i]); // Присваиваем точку ближайшему к ней кластеру
+        }
+    }
+    // Обновляем центры кластеров
+    void UpdateClustersCentres()
+    {
+        for (int i = 0; i < ClustersCount; i++) //Устанавливаем новый центр для каждого кластера
+        {
+            _clusters[i].UpdateCenterPoint();
+        }
+    }
+public:
+    KMeans(vector<Point> points, vector<Cluster> clusters)
+    {
+        _points = points;
+        _clusters = clusters;
+    }
+    vector<Point> GetPoints()
+    {
+        return _points;
+    }
+    void SetPoints(vector<Point> points)
+    {
+        _points = points;
+    }
+    vector<Cluster> GetClusters()
+    {
+        return _clusters;
+    }
+    void SetClusters(vector<Cluster> clusters)
+    {
+        _clusters = clusters;
+    }
+    // Запуск алгоритма
+    void Execute()
+    {
+        vector<Cluster> previousClusters = _clusters;
+        int iterationCounter = 0;
+        while (true)
+        {
+            iterationCounter++;
+            AssignPoints();             // Присваиваем точки к ближайшему кластеру
+            UpdateClustersCentres();    // Обновляем центры кластеров
+            int counter = 0;
+            for (int i = 0; i < ClustersCount; i++) // Проверка на схожесть текущих центров с предыдущими
+            {
+                if (previousClusters[i].GetCenterPoint() == _clusters[i].GetCenterPoint())
+                    counter++;
+            }
+            if (counter == ClustersCount) // Если центры одинаковы алгоритм завершает работу
+                break;
+
+            if (iterationCounter == maxIterations) // Ограничение по количеству итераций
+                break;
+
+            previousClusters = _clusters;
+        }
+
+        cout << "Done, iterations count " << iterationCounter << endl;
+    }
+
+    void ExecuteWithOutput()
+    {
+        //AssignPoints();
+        //PrintClustersPoints();
+
+        vector<Cluster> previousClusters = _clusters;
+        int iterationCounter = 0;
+        while (true)
+        {
+            iterationCounter++;
+            float startTime = clock();
+            AssignPoints();             // Присваиваем точки к ближайшему кластеру
+            UpdateClustersCentres();    // Обновляем центры кластеров
+            float finishTime = clock();
+            float iterationTime = (finishTime - startTime) / 1000;
+            cout << "\t" << iterationCounter << " Iteration time " << iterationTime << "s" << endl;
+            
+            // Записиваем в файл время итерации
+            WriteToFile(to_string(iterationCounter) + ":" + to_string(iterationTime) + "\n", outputFile);
+
+            for (int i = 0; i < ClustersCount; i++)
+            {
+                cout << i + 1 << " cluster have " << _clusters[i].GetPointsCount() << " points" << endl;
+            }
+
+            int counter = 0;    // Счётчик одинаковых кластеров
+            for (int i = 0; i < ClustersCount; i++) // Проверка на схожесть текущих центров с предыдущими
+            {
+                if (previousClusters[i].GetCenterPoint() == _clusters[i].GetCenterPoint())
+                    counter++;
+            }
+            if (counter == ClustersCount) // Если центры одинаковы алгоритм завершает работу
+                break;
+
+            if (iterationCounter == maxIterations) // Ограничение по количеству итераций
+                break;
+
+            previousClusters = _clusters;
+        }
+
+        //PrintClustersPoints();
+
+        cout << "Done, iterations count " << iterationCounter << endl;
+    }
+
+    void PrintClustersPoints()
+    {
+        for (int i = 0; i < ClustersCount; i++)
+        {
+            _clusters[i].ViewPoints();
+        }
+    }
+
+    void WriteToFile(string str, string path)
+    {
+        ofstream outFile(path, fstream::app);
+        if (outFile.is_open())
+        {
+            outFile << str;
+            outFile.close();
+        }
     }
 };
 
 // Получить случайные координаты
-float* GetRandomPosition()
+vector<float> GetRandomPosition(int maxLength)
 {
-    float position[countOfDimensions];
-    for (int i = 0; i < countOfDimensions; i++)
+    vector<float> position;
+    for (int i = 0; i < DimensionsCount; i++)
     {
-        position[i] = rand() % 250;
+        position.push_back(rand() % maxLength);
     }
     return position;
 }
 // Получить точку со случайными координатами
-Point GetRandomPoint()
+Point GetRandomPoint(int maxLength)
 {
-    Point p;
-    p.SetPosition(GetRandomPosition());
-    return p;
+    return Point(GetRandomPosition(maxLength));
 }
-
-//old
-float CalculateDistance(vector<float>& point1, vector<float>& point2) { // подсчет растояние между точками
-    float distance = 0.0;
-    for (int i = 0; i < countOfDimensions; i++) {
-        distance += pow((point2[i] - point1[i]), 2);
-    }
-    return sqrt(distance);
-}
-
-// Считает расстояние между точками
-float CalculateDistance(Point firstPoint, Point secondPoint) 
-{
-    float distance = 0; 
-    for (int i = 0; i < countOfDimensions; i++)    //считает расстояние между каждым измерением точек
-        distance += pow(firstPoint.GetPosition()[i] - secondPoint.GetPosition()[i], 2);
-    distance = sqrt(distance);
-    return distance;
-}
-
-//old
-// присваиваем точки к кластеру
-void AssignToClusters(vector<vector<float>>& points, vector<vector<float>>& centroids, vector<int>& clusterAssignments) {
-    for (int i = 0; i < countOfPoints; i++) {
-        float minDistance = numeric_limits<float>::max(); // берем значения что было с чем сравнивать
-        int closestCentroid = -1;
-        for (int j = 0; j < countOfClusters; j++) {
-            float distance = CalculateDistance(points[i], centroids[j]);// считаем
-            if (distance < minDistance) { // если ближе запомнаем
-                minDistance = distance;
-                closestCentroid = j;
-            }
-        }
-        clusterAssignments[i] = closestCentroid;
-    }
-}
-
-// Присваиваем точки к ближайшему кластеру
-void AssignToClusters(vector<Point>& points, vector<Cluster>& clusters)
-{
-    for (int i = 0; i < countOfClusters; i++) //Очищаем существующие точки кластеров, чтобы их переприсвоить
-        clusters[i].ClearPoints();
-
-    for (int i = 0; i < countOfPoints; i++)
-    {
-        float minDistance = 99999999999; 
-        int closestClusterKey = 0;
-        for (int j = 0; j < countOfClusters; j++)
-        {
-            float distance = CalculateDistance(points[i], clusters[j].GetCenterPoint()); // Считаем расстояние между точкой и центром кластера
-            if (distance < minDistance) // Если расстояние до кластера меньше чем уже существующее запоминаем его
-            {
-                minDistance = distance;
-                closestClusterKey = j;
-            }
-        }
-        clusters[closestClusterKey].AddPoint(points[i]); // Присваиваем кластеру который ранее запомнили точку с текущей итерации цикла
-    }
-}
-//old
-// считаем новые координаты центроидов
-void updateCentroids(vector<vector<float>>& points, vector<vector<float>>& centroids, vector<int>& clusterAssignments) {
-    for (int i = 0; i < countOfClusters; i++) {
-        vector<float> newCentroid(countOfDimensions);
-        int count = 0;
-        for (int j = 0; j < countOfPoints; j++) {
-            if (clusterAssignments[j] == i) {
-                for (int l = 0; l < countOfDimensions; l++) {
-                    newCentroid[l] += points[j][l];
-                }
-                count++;
-            }
-        }
-        if (count > 0) {
-            for (int l = 0; l < countOfDimensions; l++) {
-                newCentroid[l] /= count; // делим на количество точек в кластере
-            }
-            centroids[i] = newCentroid;
-        }
-    }
-}
-// Обновляем центры кластеров
-void UpdateClustersCentres(vector<Cluster>& clusters)
-{
-    for (int i = 0; i < countOfClusters; i++) //Устанавливаем новый центр для каждого кластера
-    {
-        clusters[i].SetCenterPoint(clusters[i].CalculateCenter());
-    }
-    //return clusters;
-}
-
-void kMeans(vector<vector<float>>& points, vector<vector<float>>& centroids, vector<float>& iterationTimes) {
-    vector<int> OldclusterAssignments(countOfPoints);
-    int count = 0;
-    while (true) {
-        float s = clock();
-        vector<int> clusterAssignments(countOfPoints);
-        AssignToClusters(points, centroids, clusterAssignments);
-        updateCentroids(points, centroids, clusterAssignments);
-        float e = clock();
-        iterationTimes.push_back((e - s) / CLOCKS_PER_SEC);
-        cout << "iteration " << count << endl;
-        int countPoints = 0;
-        for (int i = 0; i < countOfClusters; i++) {
-            cout << "cluster " << i << ":" << endl;
-            countPoints = 0;
-            for (int j = 0; j < countOfPoints; j++) {
-                if (clusterAssignments[j] == i) {
-                    countPoints++;
-                    cout << "point " << j << ": ";
-                    for (int k = 0; k < countOfDimensions; k++) {
-                        cout << points[j][k] << " ";
-                    }
-                    cout << endl;
-                }
-            }
-            cout << "count points in cluster: " << countPoints << endl;
-        }
-        cout << "\n\n\n";
-        if (clusterAssignments == OldclusterAssignments) { // если ничего не поменялось
-            cout << "True" << count << endl;
-            ofstream outFile("../../CountIteration.txt");//открываем файл
-            if (outFile.is_open()) {
-                outFile << count;
-                outFile.close();//закрываем файл
-            }
-            else {
-                cout << "Error, could not open file.";
-            }
-            break;
-        }
-        OldclusterAssignments = clusterAssignments;
-        count++;
-    }
-}
-
 
 int main() {
 
-    Point p1;
-    p1.SetPosition(GetRandomPosition());
-    Point p2;
-    p2.SetPosition(GetRandomPosition());
-    cout << CalculateDistance(p1, p2) << endl;
+    int maxDimensionLength = 400;
+
     vector<Point> points;
-    for (int i = 0; i < countOfPoints; i++)
+    for (int i = 0; i < PointsCount; i++)
     {
-        Point p;
-        p.SetPosition(GetRandomPosition());
-        points.push_back(p);
+        points.push_back(GetRandomPoint(maxDimensionLength));
     }
-    /*Cluster c;
-    c.AddPoint(p1);
-    c.AddPoint(p2);
-    c.ViewPoints();*/
-
     vector<Cluster> clusters;
-    //vector<Point> points;
-
-    for (int i = 0; i < countOfClusters; i++)
+    for (int i = 0; i < ClustersCount; i++)
     {
         Cluster c;
-        c.SetCenterPoint(GetRandomPoint());
+        c.SetCenterPoint(GetRandomPoint(maxDimensionLength));
         clusters.push_back(c);
     }
 
-    AssignToClusters(points, clusters);
-
-    for (int i = 0; i < countOfClusters; i++)
-    {
-        cout << i << " cluster have " << clusters[i].GetPointsCount() << " points" << endl;
-    }
-
-    UpdateClustersCentres(clusters);
-    AssignToClusters(points, clusters);
-    for (int i = 0; i < countOfClusters; i++)
-    {
-        cout << i << " cluster have " << clusters[i].GetPointsCount() << " points" << endl;
-    }
-
-    //cout << CalculateDistance(p1, p2) << endl;
-
-    //cout << CalculateDistance(p1.GetPosition(), p2.GetPosition()) << endl;
-    //vector<vector<float>> points(amountOfPoints, vector<float>(numberOfDimensions));
-    //vector<vector<float>> centroids(numberOfClusters, vector<float>(numberOfDimensions));
-
-    //vector<float> iterationTimes;
-    //for (int i = 0; i < amountOfPoints; i++) { // ставим рандомно точки
-    //    for (int j = 0; j < numberOfDimensions; j++) {
-    //        points[i][j] = 1 + rand() % 100;
-    //    }
-    //}
-    //for (int i = 0; i < numberOfClusters; i++) { // так же рандомно кластеры
-    //    for (int j = 0; j < numberOfDimensions; j++) {
-    //        centroids[i][j] = 1 + rand() % 100;
-    //    }
-    //}
-    //float s = clock();//начало отсчета
-    //kMeans(points, centroids, iterationTimes);
-    //float e = clock();//конец отсчета
-    //cout << "K-Means Algorithm Execution Time: " << (e - s) / CLOCKS_PER_SEC << " s" << endl;//время выполнения
-    //ofstream outFile("../../IterationTimes.txt");//открываем файл
-    //if (outFile.is_open()) {
-    //    for (int i = 0; i < iterationTimes.size(); i++) {//записываем в него
-    //        outFile << iterationTimes[i] << " ";
-    //    }
-    //    outFile.close();//закрываем файл
-    //}
-    //else { // если не получилось открыть файл
-    //    cout << "Error, could not open file.";
-    //}
-    //return 0;
+    KMeans K(points, clusters);
+    float startTime = clock();
+    K.ExecuteWithOutput();
+    float finishTime = clock();
+    float algorithmTime = (finishTime - startTime) / 1000;
+    cout << "   Execution time " << algorithmTime << "s";
+    K.WriteToFile("final:" + to_string(algorithmTime), outputFile);
 }
 
